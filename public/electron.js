87b3af37abe3da683855f975b4bbca84c8c25cac
Matthew Peveler <matt.peveler@gmail.com>
2021-12-31 13:01:59 -0500

    get a notarized app working
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow, session } = require('electron');
 const path = require('path');
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development';
 
 function createWindow() {
   // Create the browser window.
@@ -14,24 +14,24 @@ function createWindow() {
   mainWindow.loadURL(
     isDev
       ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  )
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  );
 
   if (isDev) {
     mainWindow.webContents.openDevTools({ mode: 'detach' });
   }
 }
 
-app.on("ready", () => {
+app.on('ready', () => {
   createWindow();
 
-  app.on("activate", function () {
+  app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
